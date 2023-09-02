edece60f0febf793df26d1e0aa898c44f13d5127
Aleksei <bestuzhev.aleksei.m@gmail.com>
2023-09-03 02:24:46 +0400

    upd: add RadioGroup

---
+++ src/components/ui/radio-group/radio-group.stories.tsx
@@ -0,0 +1,59 @@
+import { useState } from 'react'
+
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { Option, RadioGroup } from './'
+
+const meta = {
+  title: 'Components/Radio Group',
+  component: RadioGroup,
+  tags: ['autodocs'],
+  argTypes: {
+    // variant: {
+    //   options: ['primary', 'secondary', 'tertiary', 'link'],
+    //   control: { type: 'radio' },
+    // },
+  },
+} satisfies Meta<typeof RadioGroup>
+
+const baseData: Option[] = [
+  { label: 'First', value: '1' },
+  { label: 'Second', value: '2' },
+  { label: 'Third', value: '3' },
+]
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const DefaultControlledGroup: Story = {
+  render: args => {
+    const [value, setValue] = useState('1')
+
+    return (
+      <>
+        <RadioGroup {...args} value={value} onValueChange={setValue} />
+        <div style={{ marginTop: '10px' }}>Selected value: {value}</div>
+      </>
+    )
+  },
+
+  args: {
+    options: baseData,
+  },
+}
+
+export const DisabledGroup: Story = {
+  args: {
+    options: baseData,
+    value: '1',
+    disabled: true,
+  },
+}
+
+export const GroupWithError: Story = {
+  args: {
+    options: baseData,
+    value: '1',
+    errorMessage: 'Some error occurred',
+  },
+}
---
+++ src/components/ui/radio-group/radio-group.tsx
@@ -0,0 +1,43 @@
+import { FC } from 'react'
+
+import * as RadioGr from '@radix-ui/react-radio-group'
+import { clsx } from 'clsx'
+
+import s from './radio-group.module.scss'
+
+import { Typography } from '@/components/ui/typography'
+
+export type Option = {
+  label: string
+  value: string
+}
+
+type RadioGroupProps = {
+  options: Option[]
+  value?: string
+  onValueChange?: (value: string) => void
+  errorMessage?: string
+  disabled?: boolean
+}
+
+export const RadioGroup: FC<RadioGroupProps> = ({ options, errorMessage, ...rest }) => {
+  const labelClasses = clsx(s.item, rest.disabled && s.disabled)
+
+  return (
+    <RadioGr.Root aria-label={'Aria label'} {...rest} className={s.root}>
+      {options.map(el => (
+        <Typography as={'label'} variant={'body2'} key={el.value} className={labelClasses}>
+          <RadioGr.Item value={el.value} className={s.radio}>
+            <RadioGr.Indicator className={s.indicator} />
+          </RadioGr.Item>
+          {el.label}
+        </Typography>
+      ))}
+      {errorMessage && (
+        <Typography variant={'caption'} className={s.error}>
+          {errorMessage}
+        </Typography>
+      )}
+    </RadioGr.Root>
+  )
+}
